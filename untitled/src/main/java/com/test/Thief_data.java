2402f189c1a523a50b7223f1fae7a3bfc0622371
Adarsh <Adarsh.Singh@adform.com>
2023-07-06 10:30:46 +0530

    Removed HashMap

--- untitled/src/main/java/com/test/Thief_data.java
+++ untitled/src/main/java/com/test/Thief_data.java
@@ -3,35 +3,13 @@ package com.test;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Thief_data {
     public static int thief(String s) {
-        int count = 0;
-        boolean flag;
-        int c_count = 0;
-        int s_count = 0;
-        HashMap<String,Integer> hs = new HashMap<>();
+        int count = 1;
         char[] arr = s.toCharArray();
-        for(char c:arr){
-            if (c=='0'){
-                hs.put("0",c_count+1);
-                c_count += 1;
-            }
-            else{
-                hs.put("1",s_count+1);
-                s_count += 1;
-            }
-        }if (hs.get("0") != null) {
-            if (arr.length == hs.get("0")) {
-                count = 1;
-            }
-        }
-        if (arr[0] == '1')
-            flag = false;
-        else
-            flag = true;
+        boolean flag = (arr[0] == '1') ? false:true;
         for (char c : arr) {
             if (flag && c == '1') {
                     count += 1;
